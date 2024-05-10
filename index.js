d5345f2035ac4c635f53891def412c6801ea00cd
asfas12fas <js1tameemgt@hotmail.com>
2024-05-10 19:42:31 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function isPalindrome(str) {
-  return str === str.split("").reverse().join("");
+function canJump(nums) {
+  let maxJump = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxJump) return false;
+    maxJump = Math.max(maxJump, i + nums[i]);
+    if (maxJump >= nums.length - 1) return true;
+  }
+  return false;
 }
