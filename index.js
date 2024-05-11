02ed882f7f4e47137bfd52d2d77d698a80be7a8c
asfas12fas <js1tameemgt@hotmail.com>
2024-05-11 14:32:29 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function canJump(nums) {
-  let maxJump = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (i > maxJump) return false;
-    maxJump = Math.max(maxJump, i + nums[i]);
-    if (maxJump >= nums.length - 1) return true;
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there are elements remaining
+  while (currentIndex !== 0) {
+    // Pick a remaining element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // Swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
-  return false;
+
+  return array;
 }
+
+const shuffledDeck = shuffle([1, 2, 3, 4, 5]);
+console.log(shuffledDeck);
